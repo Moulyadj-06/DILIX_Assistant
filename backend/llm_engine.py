f04c98d2a51c0ed73d59837750c72d792202ee88
Moulyadj-06 <122588870+Moulyadj-06@users.noreply.github.com>
2026-01-07 21:39:24 +0530

    Update llm_engine.py

--- backend/llm_engine.py
+++ backend/llm_engine.py
@@ -1,37 +1,4 @@
 
-# from groq import Groq
-# import os
-# from dotenv import load_dotenv
-
-# load_dotenv()
-
-# groq_api = os.getenv("GROQ_API_KEY")
-# client = Groq(api_key=groq_api)
-
-# def get_llm_response(prompt: str) -> str:
-#     completion = client.chat.completions.create(
-#         model="llama-3.1-8b-instant",
-#         messages=[
-#             {
-#                 "role": "system",
-#                 "content": (
-#                     "You are DILIX, an AI assistant. "
-#                     "Answer the user's question directly and clearly. "
-#                     "Do NOT introduce yourself unless the user explicitly asks."
-#                 )
-#             },
-#             {
-#                 "role": "user",
-#                 "content": prompt
-#             }
-#         ],
-#         temperature=0.7,
-#         max_completion_tokens=512
-#     )
-#     # Access the content correctly
-#     return completion.choices[0].message.content
-
-
 from groq import Groq
 import os
 from dotenv import load_dotenv
